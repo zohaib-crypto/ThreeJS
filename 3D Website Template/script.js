534be45a0cc4ed38b1f332bb49e93837835a35e4
zohaib-crypto <khanzaibi191@gmail.com>
2025-03-05 14:42:15 +0000

    CONFLICT RESOLVED
    template ready

--- 3D Website Template/script.js
+++ 3D Website Template/script.js
@@ -3,13 +3,14 @@ var scene, camera, renderer, box;
 init();
 function init() {
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xFAF9F6);
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 3;
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const light = new Three.DirectionalLight();
+    const light = new THREE.DirectionalLight();
     light.position.set(0, 1, 2);
     scene.add(light);
 

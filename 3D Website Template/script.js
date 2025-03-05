71001521e39e59a9437695eff4fca34cfd193f6c
zohaib-crypto <khanzaibi191@gmail.com>
2025-03-05 16:53:04 +0000

    modify it (model not showing)
    modify the code since no model is being shown on coke.html

--- 3D Website Template/script.js
+++ 3D Website Template/script.js
@@ -1,45 +1,86 @@
-var scene, camera, renderer, box;
+var scene, camera, renderer, box, clock, mixer, action = [], mode;
 
 init();
 function init() {
+
+
+    const assetPath =  './';
+
+    clock = new THRRE.clock();
+
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xFAF9F6);
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 3;
+
+    camera = new THREE.PerspectiveCamera(60 , window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(-5, 25, 20);
+
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    //Lighting
+    const ambient = new THREE.HemisphereLight(0xffffbb, 0x0808020, 1);
+    scene.add(ambient);
+
     const light = new THREE.DirectionalLight();
-    light.position.set(0, 1, 2);
+    light.position.set(0, 10, 2);
     scene.add(light);
 
-    const material = new THREE.MeshStandardMaterial(({ color: new THREE.Color('skyblue') }));
+    //Orbit control
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.target.set(1,2,0);
+    controls.update();
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    box = new THREE.Mesh(geometry, material);
-    box.position.x = 0;
-    scene.add(box);
+    mode = 'open';
+    const btn = document.getElementById("btn");
+    btn.addEventListener('click', function() {
+        if (actions.length === 2) {
+            if(mode==="open"){
+                actions.forEach(action =>{
+                    actions.timeScale = 1.;
+                    actions.reset();
+                    actions.play();
+                })
+            }
+        }
+    })
 
+   
+
+
+    //GLFT loader
+    const loader = new THREE.GLFTLoader();
+    loader.load(assetPath + 'assets/3d models/ring open.glb', function(gltf){
+        const model = gltf.scene;
+        scene.add(model);
+    
+
+    mixer = new THREE.AnimationMixer(model);
+    const animations = gltf.animations;
+
+    animations.forEach(clip=> {
+        const action = mixer.clipAction(clip);
+        actions.push(action);
+    })
+});
+        
     window.addEventListener('resize', onResize, false);
 
+    animate();
 
-    update();
-
+    }
+    function animate(){
+        requestAnimationFrame(animate);
+        if(mixer){
+            mixer.update(clock.getDelta());
+        }
+   
+        
+renderer.render(scene,camera);
 
 }
-
-function update() {
-    requestAnimationFrame(update);
-    box.rotation.y += 0.01;
-    renderer.render(scene, camera);
- 
-}
-
 function onResize(){
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
-
-

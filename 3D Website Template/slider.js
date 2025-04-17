0a2f3a02a976ed8afe7c49612e49f54b93d8da54
zohaib-crypto <khanzaibi191@gmail.com>
2025-04-17 17:58:53 +0100

    adding navigation

--- 3D Website Template/slider.js
+++ 3D Website Template/slider.js
@@ -40,13 +40,16 @@ function initModel(path) {
   scene = new THREE.Scene();
   scene.background = null;
 
+  // Adding Camera
   camera = new THREE.PerspectiveCamera(60, container.clientWidth / container.clientHeight, 0.1, 1000);
   camera.position.set(0, 3, 5);
 
+  // Renderer
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setSize(container.clientWidth, container.clientHeight);
   container.appendChild(renderer.domElement);
 
+  // Lighting
   const ambient = new THREE.AmbientLight(0xffffff, 1.5);
   scene.add(ambient);
   const light = new THREE.DirectionalLight(0xffffff, 2);
@@ -57,21 +60,32 @@ function initModel(path) {
   controls.target.set(0, 2, 0);
   controls.update();
 
+  //Loading model
   const loader = new THREE.GLTFLoader();
   loader.load(path, function (gltf) {
     const model = gltf.scene;
     model.scale.set(2, 2, 2);
     scene.add(model);
+    // if (index === 2) {
+    //   model.scale.set(1.2, 1.2, 1.2);         // smaller scale
+    //   camera.position.set(0, 2.5, 6);         // further back
+    // } else {
+    //   model.scale.set(2, 2, 2);
+    //   camera.position.set(0, 3, 5);
+    // }
 
+    //Animation
     mixer = new THREE.AnimationMixer(model);
     gltf.animations.forEach((clip) => {
       const action = mixer.clipAction(clip);
+      action.timeScale = 0.4;
       action.play();
     });
 
     animate();
   });
 
+  // Animation
   function animate() {
     requestAnimationFrame(animate);
     if (mixer) mixer.update(0.01);
